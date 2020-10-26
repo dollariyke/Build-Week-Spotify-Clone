33dea2dbafd324dfc73c9b6e2c253de0e9695068
arzhk <72197750+arzhk@users.noreply.github.com>
2020-10-26 15:21:47 +0000

    Added functionality
    - Added background colour swap when switching tabs
    - Added wrapper for content

--- main.js
+++ main.js
@@ -10,3 +10,30 @@ function switchActive() {
 
   clickedLink.classList.add("active");
 }
+
+function switchBGColour() {
+  const aside = document.querySelector("aside");
+  const clickedLink = event.currentTarget.innerText;
+
+  switch (clickedLink) {
+    case "TRENDING":
+      aside.className = "";
+      aside.classList.add("trending-bg");
+      break;
+    case "PODCAST":
+      aside.className = "";
+      aside.classList.add("podcasts-bg");
+      break;
+    case "MOODS AND GENRES":
+      aside.className = "";
+      aside.classList.add("moods-bg");
+      break;
+    case "NEW RELEASES":
+      aside.className = "";
+      aside.classList.add("newreleases-bg");
+      break;
+    default:
+      aside.className = "";
+      aside.classList.add("discover-bg");
+  }
+}
